0e0cd552274f41a16f59e9f0585c95a05f916bb1
Mario Hvala <mario.hvala@javadev-bod.com>
2022-12-03 15:20:24 +0100

    remove console log

--- js/main.js
+++ js/main.js
@@ -252,7 +252,6 @@ function hasScrolled() {
 function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    console.log("COOKIES: ", ca)
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
